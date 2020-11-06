6e1a4c13cac0613513352b94d6b5dbd3cb30ad35
hbwang <hbwang@aibee.com>
2020-11-06 16:43:08 +0800

    1

--- src/main/scala/com/aibee/sink/WriteToMyCKExample.scala
+++ src/main/scala/com/aibee/sink/WriteToMyCKExample.scala
@@ -9,7 +9,7 @@ import org.apache.flink.connector.jdbc.{JdbcConnectionOptions, JdbcExecutionOpti
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
 
-
+//将流中数据赋值给sql语句的valuse
 class CkSinkBuilder extends JdbcStatementBuilder[(String,Int, String)] {
   def accept(ps: PreparedStatement, v: (String,Int, String)): Unit = {
     ps.setString(1, v._1)
@@ -29,19 +29,18 @@ object WriteToMyCKExample {
      (x.id, tem.toInt, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(x.timestamp))
       // (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(x.timestamp),x.temperature)
     })
-    stream.print("测试数据")
-    //jdbc test01
-   // stream.addSink(new MyJDBCSink)
-    //jdbs test02
+    //stream.print("测试数据")
+
    val sql="insert into test.t_temperature(id,temperature,create_time)values(?,?,?)"
-/*   stream.addSink(JdbcSink.sink[(String,Int, String)](sql,new CkSinkBuilder,new JdbcExecutionOptions.Builder().withBatchSize(5).build(),
+    //withbatchsize用来设定批次写入CK的条数
+   stream.addSink(JdbcSink.sink[(String,Int, String)](sql,new CkSinkBuilder,new JdbcExecutionOptions.Builder().withBatchSize(5).build(),
       new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
         .withUrl("jdbc:clickhouse://172.16.244.123:8123")
         .withDriverName("ru.yandex.clickhouse.ClickHouseDriver")
         .withUsername("default")
         .withPassword("123456")
         .build()
-    ))*/
+    ))
     env.execute()
   }
 }
